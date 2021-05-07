d0a30425cd4bada5144a0d1af8187bc2bf96f768
Christoffer <csoas20@student.aau.dk>
2021-05-07 14:50:22 +0200

    Mere fixed!

--- src/greenhouse-sim.cpp
+++ src/greenhouse-sim.cpp
@@ -22,6 +22,36 @@ int main(int argc, char const *argv[])
     clock_time.setFillColor(sf::Color::White);
     std::stringstream ss;
 
+    sf::Texture plant_sprite;
+    plant_sprite.loadFromFile("C:/Users/chri3/CLionProjects/greenhouse-sim/src/Plant1T.png");
+    ss.str(std::string());
+    sf::Sprite Plant1(plant_sprite);
+    Plant1.setPosition(36.f,177.f);
+    Plant1.scale(0.35f,0.35f);
+
+    // Plant 2 sprite (xd)
+    sf::Texture plant_sprite2;
+    plant_sprite2.loadFromFile("C:/Users/chri3/CLionProjects/greenhouse-sim/src/Plant2T.png");
+    ss.str(std::string());
+    sf::Sprite Plant2(plant_sprite2);
+    Plant2.setPosition(26.f,173.f);
+    Plant2.scale(0.35f,0.35f);
+
+    // Plant 3 sprite ( :D )
+    sf::Texture plant_sprite3;
+    plant_sprite3.loadFromFile("C:/Users/chri3/CLionProjects/greenhouse-sim/src/Plant3T.png");
+    ss.str(std::string());
+    sf::Sprite Plant3(plant_sprite3);
+    Plant3.setPosition(26.f,164.f);
+    Plant3.scale(0.35f,0.35f);
+
+    // Plant 4 sprite ( :( )
+    sf::Texture plant_sprite4;
+    plant_sprite4.loadFromFile("C:/Users/chri3/CLionProjects/greenhouse-sim/src/Plant4T.png");
+    ss.str(std::string());
+    sf::Sprite Plant4(plant_sprite4);
+    Plant4.setPosition(24.f,120.f);
+    Plant4.scale(0.35f,0.35f);
 
     while (window.isOpen()) {
 
@@ -43,45 +73,38 @@ int main(int argc, char const *argv[])
         ss.str(std::string());
         sf::Sprite Green(greenhouse_sprite);
         sf::Time elapTime1 = deltaClock.getElapsedTime();
-        int minutes = elapTime1.asSeconds() / 0.1;
-            if (minutes == 60) {
-                minutes = elapTime1.asSeconds() / 1;
-                hour++;
-                deltaClock.restart();
+        int minutes = elapTime1.asSeconds() / 0.05;
+        if (minutes == 60) {
+            minutes = elapTime1.asSeconds() / 1;
+            hour++;
+            deltaClock.restart();
 
 
-            }
-            if (hour == 24) {
-                hour = 0;
-            }
-
-
-        // Plant1 sprite (perfekt)
-
-        sf::Texture plant_sprite;
-        plant_sprite.loadFromFile("C:/Users/chri3/CLionProjects/greenhouse-sim/src/Plant1T.png");
-        ss.str(std::string());
-        sf::Sprite Plant1(plant_sprite);
-        Plant1.setPosition(36.f,177.f);
-        Plant1.scale(0.35f,0.35f);
-
-        // Plant 2 sprite (xd)
-        sf::Texture plant_sprite2;
-        plant_sprite2.loadFromFile("C:/Users/chri3/CLionProjects/greenhouse-sim/src/Plant2T.png");
-        ss.str(std::string());
-        sf::Sprite Plant2(plant_sprite2);
-        Plant2.setPosition(26.f,173.f);
-        Plant2.scale(0.35f,0.35f);
-        
-        // Plant 3 sprite (:D)
-        
-
+        }
+        if (hour == 24) {
+            hour = 0;
+        }
+        window.draw(Green);
+        if(hour >= 1 && hour < 2)
+        {
+            window.draw(Plant1);
+        }
+        if(hour >= 2 && hour < 3)
+        {
+            window.draw(Plant2);
+        }
+        if(hour >= 3 && hour < 4)
+        {
+            window.draw(Plant3);
+        }
+        if(hour >= 4)
+        {
+            window.draw(Plant4);
+        }
 
         ss << std::to_string(hour) << ':' << std::to_string(minutes);
         clock_time.setString(ss.str());
-        window.draw(Green);
-        window.draw(Plant1);
-        window.draw(Plant2);
+
         window.draw(clock_time);
 
         // end the current frame
